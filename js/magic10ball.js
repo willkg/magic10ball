e40399f604004b54475602c6127a2018fc4ba926
Ricky Rosario <rickyrosario@gmail.com>
2012-05-03 12:11:30 -0700

    random background

--- js/magic10ball.js
+++ js/magic10ball.js
@@ -10,16 +10,27 @@ var messages = [
 function randomMessage() {
     return messages[Math.round(Math.random() * messages.length)];
 }
+
 function init() {
     document
         .getElementById('shake-it')
         .addEventListener('click', function() {
+            document.body.style.background = '#'+Math.floor(Math.random()*16777215).toString(16);
             document
             	.getElementById('message')
             	.innerHTML = randomMessage();
 
         });
+
+    /* TODO:
+    window
+        .addEventListener(
+            'devicemotion',
+            function() {
+                alert('shaking');
+            });*/
 }
+
 window.addEventListener('load', function ballLoad(evt) {
 	window.removeEventListener('load', ballLoad);
 	init();
