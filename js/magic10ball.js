de68af48ce7910824b745d9d9e49da607d3d8695
Will Kahn-Greene <willg@bluesock.org>
2012-05-03 12:30:07 -0700

    Add save/load messages

--- js/magic10ball.js
+++ js/magic10ball.js
@@ -1,14 +1,38 @@
 "use strict";
 
-var messages = [
-    'yes',
-    'no',
-    'maybe'
-    //...
-];
-
+var STORAGE_KEY = 'magic10ball_messages';
+var default_messages = ['yes', 'no', 'maybe'];
+function getMessages() {
+    var messages = window.localStorage.getItem(STORAGE_KEY);
+    if (messages == null) {
+        messages = default_messages.join('\n');
+    }
+    if (messages.length == 0) {
+        messages = default_messages.join('\n');
+    }
+    return messages.split('\n');
+}
 function randomMessage() {
-    return messages[Math.round(Math.random() * messages.length)];
+    var messages = getMessages();
+    return messages[Math.round(Math.random() * (messages.length - 1))];
+}
+function saveMessages() {
+    var data = document.getElementById('messages-textbox').value;
+    window.localStorage.setItem(STORAGE_KEY, data);
+}
+function swap() {
+    var children = document.body.children;
+    for (var i=0; i < children.length; i++) {
+        var el = children[i];
+        if (!el.classList.contains('view')) {
+            continue;
+        }
+        if (el.classList.contains('hidden')) {
+            el.classList.remove('hidden');
+        } else {
+            el.classList.add('hidden');
+        }
+    }
 }
 
 function init() {
@@ -21,7 +45,6 @@ function init() {
             	.innerHTML = randomMessage();
 
         });
-
     /* TODO:
     window
         .addEventListener(
@@ -29,6 +52,15 @@ function init() {
             function() {
                 alert('shaking');
             });*/
+    document
+        .getElementById('messages-textbox')
+        .value = getMessages().join('\n');
+    document
+        .getElementById('save-button')
+        .addEventListener('click', saveMessages);
+    document
+        .getElementById('settings-button')
+        .addEventListener('click', swap);
 }
 
 window.addEventListener('load', function ballLoad(evt) {
